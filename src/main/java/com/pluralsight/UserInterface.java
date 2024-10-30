a4fba05d9de993b634b7dc09f7dd1dfa0724bc04
Yigit <yigistaff@gmail.com>
2024-10-30 11:27:39 -0400

    In UserInterface class skeleton code has been created.

--- src/main/java/com/pluralsight/UserInterface.java
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -1,4 +1,39 @@
 package com.pluralsight;
 
 public class UserInterface {
+
+    private DealerShip dealerShip;
+
+    public UserInterface() {
+
+    }
+
+    public void processGetByPriceRequest() {
+
+    }
+    public void processGetByYearRequest() {
+
+    }
+    public void processGetByColorRequest() {
+
+    }
+    public void processGetByMileageRequest() {
+
+    }
+    public void processGetByVehicleTypeRequest() {
+
+    }
+    public void processGetAllVehiclesRequest() {
+
+    }
+    public void processAddVehicleRequest() {
+
+    }
+    public void processRemoveVehicleRequest() {
+
+    }
+
+
+
+
 }
