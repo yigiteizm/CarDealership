b9036bacdb4f25a4bed7764a47b1e54ba82a6728
Yigit <yigistaff@gmail.com>
2024-10-30 15:10:47 -0400

    userInterface has been added to program class.

--- src/main/java/com/pluralsight/Program.java
+++ src/main/java/com/pluralsight/Program.java
@@ -1,2 +1,10 @@
 package com.pluralsight;
 
+public class Program {
+
+    public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        userInterface.disPlay();
+
+    }
+}
